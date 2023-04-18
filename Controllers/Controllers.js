20a8a7845ddf19f332717defc314f45f9e6ccb38
Shubhrant05 <shubhrantyadav34366@gmail.com>
2023-04-18 15:18:57 +0530

    completed db connection

--- Controllers/Controllers.js
+++ Controllers/Controllers.js
@@ -1,21 +1,25 @@
-// write controller function in this file 
+// write controller function in this file
 
-const Pool = require('pg').Pool
-const pool = new Pool({
-  user: 'postgres',
+const pg = require('pg')
+const client = new pg.Client({
+  user: 'fusion_admin',
   host: 'localhost',
   database: 'institute_website',
-  password: 'password',
-  port: 5000,
+  password: 'hello123',
+  port: 5432,
 })
-
-//get all users
+try {
+  client.connect()
+} catch (error) {
+  console.log("Client not connect",error)
+}
 const getUsers = (request, response, next) => {
-    pool.query('SELECT * FROM Administration', (error, results) => {
+    client.query('SELECT * FROM administration', (error, results) => {
       if (error) {
         console.log(error)
       }
-      response.status(200).json(results.rows)
+      console.log(response,"response")
+      response.status(200).json(results)
     })
   }
 
