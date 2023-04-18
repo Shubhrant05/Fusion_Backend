0877dc28377f4b49f3d92511d64985afe5e3c18b
nandani-patel <nandanip271@gmail.com>
2023-04-18 14:22:31 +0530

    db connection and get user api

--- Controllers/Controllers.js
+++ Controllers/Controllers.js
@@ -1,1 +1,24 @@
 // write controller function in this file 
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'institute_website',
+  password: 'password',
+  port: 5000,
+})
+
+//get all users
+const getUsers = (request, response, next) => {
+    pool.query('SELECT * FROM Administration', (error, results) => {
+      if (error) {
+        console.log(error)
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
+  module.exports = {
+    getUsers,
+  }
--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express')
-const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 5000
 
-const app = express();
-app.use(cors())
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
-console.log('Connected!!!')
-app.listen(5000);
+app.get('/', (request, response) => {
+    response.json({ info: 'successfull' })
+})
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
+
+const contt = require('./Controllers/Controllers');
+app.get('/users', contt.getUsers)
+
